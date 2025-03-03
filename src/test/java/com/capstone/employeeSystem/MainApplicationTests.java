490a5b53ca1b104e81fbe63d27158fb73b6af5aa
Kielo Bash Mercado <kielo_bash_mercado@dlsu.edu.ph>
2025-03-03 11:41:37 +0800

    init authentications

---
+++ src/test/java/com/capstone/employeeSystem/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.employeeSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
